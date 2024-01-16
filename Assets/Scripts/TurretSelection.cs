07cfe5d5ed4a3c05f28b7b89f42237ce3eb4fee2
Arvi-beep-boop <xarvichanx@gmail.com>
2024-01-16 22:12:26 +0100

    Selection, Display, Remove

--- Assets/Scripts/TurretSelection.cs
+++ Assets/Scripts/TurretSelection.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurretSelection : MonoBehaviour
 {
     public GameObject selectedObject;
     public Material selectMaterial;
     public Material afterMaterial; //just for now
+
+    [SerializeField] private Text displayText;
+    [SerializeField] private Image displayImage;
     void Start()
     {
         
@@ -41,11 +45,52 @@ public class TurretSelection : MonoBehaviour
         // maybe consider outline or some visual effects
         selectedObject = objToSelect;
         selectedObject.gameObject.GetComponent<Renderer>().material = selectMaterial;
+        ShowDisplay();
+
     }
 
     public void Deselect()
     {
         selectedObject.gameObject.GetComponent<Renderer>().material = afterMaterial;
         selectedObject = null;
+        ClearDisplay();
+    }
+
+    public void ShowSelectedName()
+    {
+        displayText.text = selectedObject.name;
+    }
+
+    public void ClearSelectedName() 
+    {
+        displayText.text = string.Empty;
+    }
+
+    public void RemoveTurret()
+    {
+        Destroy(selectedObject.gameObject);
+        selectedObject = null;
+        ClearDisplay();
+    }
+
+    public void ShowImage()
+    {
+       displayImage.sprite = selectedObject.GetComponent<Displays>().sprite;
+    }
+    public void HideImage()
+    {
+        displayImage.sprite = null;
+    }
+
+    public void ClearDisplay()
+    {
+        ClearSelectedName();
+        HideImage();
+    }
+
+    public void ShowDisplay()
+    {
+        ShowImage();
+        ShowSelectedName();
     }
 }
