6e1dc91ffb9425ac7fe93429d06b1c7425ed1c5d
Arvi-beep-boop <xarvichanx@gmail.com>
2024-03-17 19:06:01 +0100

    Vertical rotation, quick fix

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -72,7 +72,7 @@ public class Turret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(direction);
         Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         //Vector3 rotation = lookRotation.eulerAngles;
-        transform.rotation = Quaternion.Euler(0, rotation.y, 0);
+        transform.rotation = Quaternion.Euler(rotation.x, rotation.y, 0);
     }
 
     void Fire()
