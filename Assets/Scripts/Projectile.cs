164d97f51d8de1714035dd6d65613297af703698
Arvi-beep-boop <xarvichanx@gmail.com>
2024-04-07 18:49:35 +0200

    bug fix

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -16,7 +16,7 @@ public class Projectile : MonoBehaviour
         if(Physics.Raycast(new Ray(transform.position, transform.forward * (-1)), out RaycastHit hit, Ammo.velocity * Time.deltaTime))
         {
             transform.position = hit.point;
-            hit.collider.SendMessage("SetProjectileParent", this.transform.parent.gameObject);
+            hit.collider.SendMessage("SetProjectileParent", this.transform.parent.gameObject, SendMessageOptions.DontRequireReceiver);
             hit.collider.SendMessage("ApplyDamage", Ammo.damage, SendMessageOptions.DontRequireReceiver);
             GetComponent<MeshRenderer>().enabled = false;
             Destroy(gameObject, 1f);
