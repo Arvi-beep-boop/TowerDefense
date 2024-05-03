323b5cda9c4719d2928ae365d7f149c8db44ca8c
PLeX0 <49524128+PLeX0@users.noreply.github.com>
2024-05-03 15:50:11 +0200

    Enemy Update
    -added health bars for enemies
    -added player receiving damage and enemies disappearing when reaching the last waypoint

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     // will be changed as Scriptable Obj in the future
     public float health = 20;
+    public float maxHealth = 20;
+    public int damage = 20;
     public int moneyValue = 2; // Value in money for killing this enemy
     private GameObject projectileParent = null;
+    [SerializeField] private Image healthBar;
+    [SerializeField] private Image healthBarBackGround;
     
     // takes damage amount and source turret (one that shot) to let it know if the enemy is destroyed.
     public void ApplyDamage(int damage) 
     {
+        healthBar.enabled = true;
+        healthBarBackGround.enabled = true;
         health -= damage;
         Debug.Log("Damage applied!");
-        if (health < 0)
+        if (health <= 0)
         {
             LevelManager.instance.AddMoney(moneyValue);
             Destroy(gameObject, 0.1f);
@@ -26,5 +33,15 @@ public class Enemy : MonoBehaviour
     {
         projectileParent = parent;
     }
- 
+    private void Start()
+    {
+        maxHealth = health;
+        healthBar.enabled = false;
+        healthBarBackGround.enabled = false;
+    }
+    void Update()
+    {
+        healthBar.fillAmount = health / maxHealth;   
+    }
+
 }
